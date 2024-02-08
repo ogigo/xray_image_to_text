1d040edee0ff8eec8810001684e16e98e52eada7
ogigo <shamimahamed190@gmail.com>
2024-02-08 21:30:52 +0600

    second commit

--- dataset.py
+++ dataset.py
@@ -1,40 +1,8 @@
-import torch
-from torch.utils.data import Dataset,DataLoader
 import pandas as pd
-from inference import feature_extractor,tokenizer
-from PIL import Image
-max_length=512
+from sklearn.model_selection import train_test_split
 
 images_captions_df=pd.read_csv("data_directory")
 
-class LoadDataset(Dataset):
-    def __init__(self, df):
-        self.images = images_captions_df['imgs'].values
-        self.captions = images_captions_df['captions'].values
+train_df, test_df = train_test_split(images_captions_df, test_size=0.2, shuffle=True, random_state=42)
 
-    def __getitem__(self, idx):
-        # everything to return is stored inside this dict
-        inputs = dict()
 
-        # load the image and apply feature_extractor
-        image_path = str(self.images[idx])
-        image = Image.open(image_path).convert("RGB")
-        image = feature_extractor(images=image, return_tensors='pt')
-
-        # load the caption and apply tokenizer
-        caption = self.captions[idx]
-        labels = tokenizer(
-            caption,
-            max_length=max_length,
-            truncation=True,
-            padding='max_length',
-            return_tensors='pt',
-        )['input_ids'][0]
-
-        # store the inputs and labels in the dict we created
-        inputs['pixel_values'] = image['pixel_values'].squeeze()
-        inputs['labels'] = labels
-        return inputs
-
-    def __len__(self):
-        return len(self.images)
