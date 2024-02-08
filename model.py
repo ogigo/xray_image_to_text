ad8505e85c6381a495adaadc9120e99995d62f5c
ogigo <shamimahamed190@gmail.com>
2024-02-08 21:31:22 +0600

    second commit

--- model.py
+++ model.py
@@ -1,5 +1,18 @@
 from transformers import AutoFeatureExtractor,AutoTokenizer,VisionEncoderDecoderModel
-import torch
 
-vit_model = VisionEncoderDecoderModel.from_pretrained("kajol/xray_to_text")
 
+encoder_checkpoint = "google/vit-base-patch16-224-in21k"
+decoder_checkpoint = "gpt2"
+
+feature_extractor = AutoFeatureExtractor.from_pretrained(encoder_checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(decoder_checkpoint)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(
+    encoder_checkpoint,
+    decoder_checkpoint
+)
+model.config.decoder_start_token_id = tokenizer.bos_token_id
+model.config.pad_token_id = tokenizer.pad_token_id
+# model.config.vocab_size = model.config.decoder.vocab_size
+model.config.num_beams = 4
